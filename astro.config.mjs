bc10cb267ace52d87e60a93b1d0ffe56025627ae
Nick Karnik <nick@karnik.io>
2025-09-28 05:50:07 -0700

    Remove React integration - not needed for simple blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://theoutlander.github.io",
   base: "/",
   output: "static",
-  integrations: [react(), sitemap()],
+  integrations: [sitemap()],
 });
