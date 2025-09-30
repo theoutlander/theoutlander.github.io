aac94abfb41c7ddb0ab8ee849242b2916250925f
Nick Karnik <nick@karnik.io>
2025-09-30 00:27:32 -0700

    Refactor PostJsonLd component for improved readability: Updated the PostJsonLd component to enhance code formatting and consistency. Changed double quotes to single quotes in JSON-LD structure and improved the destructuring of props for better clarity.

--- src/components/seo/PostJsonLd.tsx
+++ src/components/seo/PostJsonLd.tsx
@@ -5,29 +5,34 @@ interface PostJsonLdProps {
   excerpt: string;
 }
 
-export default function PostJsonLd({ title, url, date, excerpt }: PostJsonLdProps) {
+export default function PostJsonLd({
+  title,
+  url,
+  date,
+  excerpt,
+}: PostJsonLdProps) {
   const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "BlogPosting",
-    "headline": title,
-    "url": url,
-    "datePublished": date,
-    "description": excerpt,
-    "author": {
-      "@type": "Person",
-      "name": "Nick Karnik",
-      "url": "https://nick.karnik.io"
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    headline: title,
+    url: url,
+    datePublished: date,
+    description: excerpt,
+    author: {
+      '@type': 'Person',
+      name: 'Nick Karnik',
+      url: 'https://nick.karnik.io',
     },
-    "publisher": {
-      "@type": "Person",
-      "name": "Nick Karnik",
-      "url": "https://nick.karnik.io"
-    }
+    publisher: {
+      '@type': 'Person',
+      name: 'Nick Karnik',
+      url: 'https://nick.karnik.io',
+    },
   };
 
   return (
     <script
-      type="application/ld+json"
+      type='application/ld+json'
       dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
     />
   );
